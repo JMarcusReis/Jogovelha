7e28890333871a5cc93bfc337f28a9039654e6b7
João Marcus <joaosenareis@gmail.com>
2024-11-09 20:37:36 -0300

    Completa o setup.py

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jogovelha',
+    version='0.1',
+    description='Um jogo da velha',
+    install_requires=[],
+    packages=find_packages()
+)
